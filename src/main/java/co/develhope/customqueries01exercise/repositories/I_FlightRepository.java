1aecbbc49c418023984c7b40d6a026f1b81170d8
Mattia <oleandromattia@gmail.com>
2022-09-01 20:39:15 +0200

    Fix annotation

--- src/main/java/co/develhope/customqueries01exercise/repositories/I_FlightRepository.java
+++ src/main/java/co/develhope/customqueries01exercise/repositories/I_FlightRepository.java
@@ -3,7 +3,6 @@ package co.develhope.customqueries01exercise.repositories;
 import co.develhope.customqueries01exercise.entities.Flight;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import org.springframework.stereotype.Service;
 
 @Repository
 public interface I_FlightRepository extends JpaRepository<Flight, Integer> {
